bf6063d82e00a8e0a9c8c7d8e7ad7a68c87dfd36
charotAmine <charotamine@gmail.com>
2020-05-15 00:52:52 +0200

    add my first application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gossipit_dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
